d640ecacb38569748bd9d57ba2552e2ce5366074
buithaihung <buithaihung6934@gmail.com>
2021-11-25 16:34:48 +0700

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/Home";
 function App() {
   return (
-    <Router>
+    // <h1>IT</h1>
+    <BrowserRouter>
       <div className="App">
         <Header />
-        <Route path="/" component={Home} />
+        <div className="container container-fluid">
+          <Routes>
+            <Route path="/" element={<Home/>} exact />
+          </Routes>
+        </div>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
