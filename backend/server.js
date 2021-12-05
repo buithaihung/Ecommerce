806afc03111e1df48eed6dffd26fda5b4ca82475
buithaihung <buithaihung6934@gmail.com>
2021-12-05 17:02:52 +0700

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,41 @@
-const app = require("./app");
-const connectDatabase = require("./config/database");
-// const dotenv = require("dotenv");
-const cloudinary = require('cloudinary');
-//seting up config file
-if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').dotenv.config({ path: "backend/config/config.env" });
+const app = require('./app')
+const connectDatabase = require('./config/database')
+
+// const dotenv = require('dotenv');
+const cloudinary = require('cloudinary')
+
+// Handle Uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down due to uncaught exception');
+    process.exit(1)
+})
+
+// Setting up config file
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+
+// dotenv.config({ path: 'backend/config/config.env' })
 
 
-
-//Connecting to database
+// Connecting to database
 connectDatabase();
-//seting up cloudinary config
+
+// Setting up cloudinary configuration
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
-const server = app.listen(process.env.PORT, () => {
-  console.log(
-    `server start on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
-  );
-});
-//Handle uncaught exceptions 
-process.on('uncaughtException',err => {
-  console.log(`ERROR: ${err.stack}`);
-  console.log('Shutting down server due to uncaught exception');
-  process.exit(1);
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-//Handle unhandled Promise rejections
-process.on("unhandledRejection", (err) => {
-  console.log(`ERROR: ${err.message}`);
-  console.log("Shutting down the server due to Unhandle Promise Rejection");
-  server.close(() => {
-    process.exit(1);
-  });
-});
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
+
+// Handle Unhandled Promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down the server due to Unhandled Promise rejection');
+    server.close(() => {
+        process.exit(1)
+    })
+})
